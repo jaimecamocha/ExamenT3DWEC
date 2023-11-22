5c8a4349c2cc975645e44651d6318dd36a27e4e3
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-11-22 09:23:38 +0100

    Línea 74: corregimos el error; ponemos el fondo de color negro

--- scripts/index.js
+++ scripts/index.js
@@ -66,7 +66,7 @@ while (balls.length < 25) { // ATENCIÓN: cambiamos el número de bolas
         random(-7, 7),
         size
     );
-
+ 
     balls.push(ball);
 }
 
@@ -82,7 +82,7 @@ function loop() {
                 ball.collisionDetect(otherBall);
             }
         }
-    }
+    } 
 
     requestAnimationFrame(loop);
 }
